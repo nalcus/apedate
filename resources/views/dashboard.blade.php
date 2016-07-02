cab2e0df29d396a9c8217891b85ea0cbf139846a
Nicholas Alcus <nalcus@gmail.com>
2016-07-02 15:30:35 -0700

    basic dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,14 @@
 @section('content')
     <section class="row new-post">
       <div class="col-md-6 col-md-offset-3">
-        <header>TODO: Dashboard</header>
+        <header>Dashboard</header>
+        username:{{ Auth::user()->username }} <br>
+        email:{{ Auth::user()->email }} <br>
+        name: {{ Auth::user()->profile->name }} <br>
+        age: {{ Auth::user()->profile->age }} <br>
+        gender: {{ Auth::user()->profile->gender }} <br>
+        location: {{ Auth::user()->profile->location }} <br>
+        bio: {{ Auth::user()->profile->bio }}
       </div>
     </section>
 @endsection
